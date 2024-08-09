8f1e7825aea55845a57057c79640ecc152dcaff0
Jarrod Watts <jarrodwattsyt@gmail.com>
2024-08-09 15:04:37 +1000

    barebones implementation

--- contracts/accounts/BasicAccount.sol
+++ contracts/accounts/BasicAccount.sol
@@ -2,65 +2,63 @@
 pragma solidity ^0.8.0;
 
 import "@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IAccount.sol";
+import {TransactionHelper} from "@matterlabs/zksync-contracts/l2/system-contracts/libraries/TransactionHelper.sol";
 
 contract BasicAccount is IAccount {
-    /// @notice Called by the bootloader to validate that an account agrees to process the transaction
-    /// (and potentially pay for it).
-    /// @param _txHash The hash of the transaction to be used in the explorer
-    /// @param _suggestedSignedHash The hash of the transaction is signed by EOAs
-    /// @param _transaction The transaction itself
-    /// @return magic The magic value that should be equal to the signature of this function
-    /// if the user agrees to proceed with the transaction.
-    /// @dev The developer should strive to preserve as many steps as possible both for valid
-    /// and invalid transactions as this very method is also used during the gas fee estimation
-    /// (without some of the necessary data, e.g. signature).
+    using TransactionHelper for *;
+
+    modifier onlyBootloader() {
+        assert(msg.sender == BOOTLOADER_FORMAL_ADDRESS);
+        _;
+    }
+
     function validateTransaction(
         bytes32 _txHash,
         bytes32 _suggestedSignedHash,
         Transaction calldata _transaction
-    ) external payable returns (bytes4 magic) {
-        // TODO
+    ) external payable onlyBootloader returns (bytes4 magic) {
+        magic = ACCOUNT_VALIDATION_SUCCESS_MAGIC;
     }
 
-    // Mandatory and will be called by the system after the fee is charged from the user
-    function executeTransaction(
-        bytes32 _txHash,
-        bytes32 _suggestedSignedHash,
-        Transaction calldata _transaction
-    ) external payable {
-        // TODO
-    }
-
-    // There is no point in providing possible signed hash in the `executeTransactionFromOutside` method,
-    // since it typically should not be trusted.
-    function executeTransactionFromOutside(
-        Transaction calldata _transaction
-    ) external payable {
-        // TODO
-    }
-
-    // Is optional and will be called by the system if the transaction has no paymaster,
-    // i.e. the account is willing to pay for the transaction.
-    // This method should be used to pay for the fees by the account.
-    // Note, that if your account will never pay any fees and will always rely on the paymaster feature, you don't have to implement this method.
-    // This method must send at least tx.gasprice * tx.gasLimit ETH to the bootloader address.
     function payForTransaction(
         bytes32 _txHash,
         bytes32 _suggestedSignedHash,
         Transaction calldata _transaction
-    ) external payable {
-        // TODO
+    ) external payable onlyBootloader {
+        bool success = _transaction.payToTheBootloader();
+        require(success, "Failed to pay the fee to the operator");
     }
 
-    // is optional and will be called by the system if the transaction has a paymaster, 
-    // i.e. there is a different address that pays the transaction fees for the user. 
-    // This method should be used to prepare for the interaction with the paymaster. 
-    // One of the notable examples where it can be helpful is to approve the ERC-20 tokens for the paymaster.
+    function executeTransaction(
+        bytes32 _txHash,
+        bytes32 _suggestedSignedHash,
+        Transaction calldata _transaction
+    ) external payable onlyBootloader {
+        address to = address(uint160(_transaction.to));
+        (bool success, ) = to.call{value: _transaction.value}(
+            _transaction.data
+        );
+        require(success, "Failed to execute the transaction");
+    }
+
+    function executeTransactionFromOutside(
+        Transaction calldata _transaction
+    ) external payable onlyBootloader {
+        bool success = _transaction.payToTheBootloader();
+        require(success, "Failed to pay the fee to the operator");
+    }
+
     function prepareForPaymaster(
         bytes32 _txHash,
         bytes32 _possibleSignedHash,
         Transaction calldata _transaction
-    ) external payable {
-        // TODO
+    ) external payable onlyBootloader {
+        _transaction.processPaymasterInput();
     }
+
+    fallback() external {
+        assert(msg.sender != BOOTLOADER_FORMAL_ADDRESS);
+    }
+
+    receive() external payable {}
 }
