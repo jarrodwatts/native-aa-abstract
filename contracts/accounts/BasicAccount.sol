5e0cae7e2fb06b6898c64efbc0d2f5fb062e55d6
Jarrod Watts <jarrodwattsyt@gmail.com>
2024-08-09 12:26:52 +1000

    Implement IAccount

--- contracts/accounts/BasicAccount.sol
+++ contracts/accounts/BasicAccount.sol
@@ -1,6 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IAccount} from "@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IAccount.sol";
+import "@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IAccount.sol";
 
-contract BasicAccount is IAccount {}
+contract BasicAccount is IAccount {
+    function validateTransaction(
+        bytes32 _txHash,
+        bytes32 _suggestedSignedHash,
+        Transaction calldata _transaction
+    ) external payable returns (bytes4 magic) {
+        // TODO
+    }
+
+    function executeTransaction(
+        bytes32 _txHash,
+        bytes32 _suggestedSignedHash,
+        Transaction calldata _transaction
+    ) external payable {
+        // TODO
+    }
+
+    // There is no point in providing possible signed hash in the `executeTransactionFromOutside` method,
+    // since it typically should not be trusted.
+    function executeTransactionFromOutside(
+        Transaction calldata _transaction
+    ) external payable {
+        // TODO
+    }
+
+    function payForTransaction(
+        bytes32 _txHash,
+        bytes32 _suggestedSignedHash,
+        Transaction calldata _transaction
+    ) external payable {
+        // TODO
+    }
+
+    function prepareForPaymaster(
+        bytes32 _txHash,
+        bytes32 _possibleSignedHash,
+        Transaction calldata _transaction
+    ) external payable {
+        // TODO
+    }
+}
