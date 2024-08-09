c36cbefb060c8797eaddcafd2bb56be4ab81e74a
Jarrod Watts <jarrodwattsyt@gmail.com>
2024-08-09 12:12:39 +1000

    use in memory node as default network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@matterlabs/hardhat-zksync";
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "abstractTestnet",
+  defaultNetwork: "inMemoryNode",
   networks: {
     abstractTestnet: {
       url: "https://api.testnet.abs.xyz",
